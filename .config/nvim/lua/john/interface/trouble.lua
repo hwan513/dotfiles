c7b316f1d794801ff6c4bfcf9c55976641f0f77a
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-08-15 17:35:24 +1200

    modified vim get references to use trouble by default

--- .config/nvim/lua/john/interface/trouble.lua
+++ .config/nvim/lua/john/interface/trouble.lua
@@ -11,7 +11,7 @@ keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
 keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
 keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
 keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
-keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
+keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
 
 trouble.setup({
 	position = "right", -- position of the list can be: bottom, top, left, right
