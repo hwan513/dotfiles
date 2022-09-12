1c726111b67c6e00f668150dcd7b3c99490bc344
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-09-12 17:33:20 +1200

    updated java code running alias

--- .config/nvim/lua/john/interface/terminal.lua
+++ .config/nvim/lua/john/interface/terminal.lua
@@ -58,7 +58,7 @@ local function CompileRun()
 		c = string.format("gcc %s -Wall -lm -o %s && time ./%s", fp, fe, fe),
 		cpp = string.format("g++ %s -Wall -lm -o %s && time ./%s", fp, fe, fe),
 		-- java = string.format("javac %s && time java %s", fp, fe),
-		java = "mej",
+		java = "mjr",
 		rust = "cargo run",
 		lua = string.format("time luajit %s", fp),
 		sh = string.format("time bash %s", fp),
