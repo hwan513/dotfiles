b17e39f0b915dcce71eb3820eb6ccf2aa201dfb3
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-09-15 09:53:47 +1200

    calling dap ui setup function

--- .config/nvim/lua/john/plugins.lua
+++ .config/nvim/lua/john/plugins.lua
@@ -164,7 +164,7 @@ return packer.startup(function(use)
   use("Pocco81/DAPInstall.nvim")
   use { "rcarriga/nvim-dap-ui",
     requires = { "mfussenegger/nvim-dap" },
-    -- config = function() require("dapui").setup() end
+    config = function() require("dapui").setup() end
   }
   use("theHamsta/nvim-dap-virtual-text")
   use("jbyuki/one-small-step-for-vimkind")
