e3cb4441da98bce6c114fedc3cae188740398ce4
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-09-17 00:23:22 +1200

    fixed codelldb get directory call

--- .config/nvim/lua/john/lsp/settings/rust_dap.lua
+++ .config/nvim/lua/john/lsp/settings/rust_dap.lua
@@ -1,6 +1,6 @@
-local extension_path = require("mason-core.package").get_install_path("codelldb") .. "/codelldb/extension/"
-local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+local extension_path = require("mason-registry").get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
 return {
   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
 }
