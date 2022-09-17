d4d868e2b6292b865334b3084124721df5e27ffe
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-09-17 12:44:02 +1200

    unsuccessfully attempted to add nvim-jdtls
    lsp config is much easier to setup for me and its features are getting
    better.

--- .config/nvim/lua/john/lsp/mason.lua
+++ .config/nvim/lua/john/lsp/mason.lua
@@ -52,6 +52,14 @@ for _, server in pairs(servers) do
 
   if server == "rust_analyzer" then
     require("rust-tools").setup({ server = opts, dap = require("john.lsp.settings.rust_dap") })
+    -- elseif server == "jdtls" then
+    --   opts = vim.tbl_deep_extend("force", opts, require("john.lsp.settings.nvim-jdtls"))
+    --   local group = vim.api.nvim_create_augroup("jdtls", { clear = true })
+    --   vim.api.nvim_create_augroup("FileType", {
+    --     pattern = "java",
+    --     callback = require("jdtls").start_or_attach(opts),
+    --     group = group,
+    --   })
   else
     lspconfig[server].setup(opts)
   end
