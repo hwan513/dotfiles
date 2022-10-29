603080489181f90b09b0634f2475214cf14d8d1c
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-10-29 22:05:25 +1300

    feat: add a survey filling automation script and keybind

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,18 +1,19 @@
 -- local basePath = os.getenv("HOME") .. "/.hammerspoon/"
 local function prequire(call)
-	local status, _ = pcall(require, call)
-	if not status then
-		hs.alert(call .. " failed to load")
-	end
+  local status, _ = pcall(require, call)
+  if not status then
+    hs.alert(call .. " failed to load")
+  end
 end
 
 hs.loadSpoon("SpoonInstall")
 -- spoon.SpoonInstall:asyncInstallSpoonFromRepo("MiroWindowsManager")
--- spoon.SpoonInstall:andUse("EmmyLua")
+spoon.SpoonInstall:andUse("EmmyLua")
 prequire("fakeServer")
 prequire("reloadConfig")
 prequire("windows")
 prequire("menuUtils")
 prequire("keyUtils")
 prequire("download")
+prequire("OKRThing")
 prequire("finderOpen")
