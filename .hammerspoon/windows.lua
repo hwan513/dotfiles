b60c54ab0b98fb5fde7e1038cf6f8d8646fc8098
hwan513 <100452681+hwan513@users.noreply.github.com>
2022-11-03 13:17:11 +1300

    feat: add window movement between monitors

--- .hammerspoon/windows.lua
+++ .hammerspoon/windows.lua
@@ -2,10 +2,24 @@ local hyper = { "ctrl", "alt", "cmd" }
 hs.loadSpoon("MiroWindowsManager")
 hs.window.animationDuration = 0
 spoon.MiroWindowsManager:bindHotkeys({
-	up = { hyper, "up" },
-	right = { hyper, "right" },
-	down = { hyper, "down" },
-	left = { hyper, "left" },
-	fullscreen = { hyper, "f" },
-	nextscreen = { hyper, "n" },
+  up = { hyper, "up" },
+  right = { hyper, "right" },
+  down = { hyper, "down" },
+  left = { hyper, "left" },
+  fullscreen = { hyper, "f" },
+  nextscreen = { hyper, "n" },
 })
+
+-- window moving for when there is multiple monitors
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "right", function()
+  -- move the focused window one display to the right
+  local win = hs.window.focusedWindow()
+  win:moveOneScreenEast()
+end)
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "left", function()
+  -- move the focused window one display to the left
+  local win = hs.window.focusedWindow()
+  win:moveOneScreenWest()
+end)
