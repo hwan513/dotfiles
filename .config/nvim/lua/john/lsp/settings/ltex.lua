634f7fe384b10b06b9cadebd58ead1db75622b70
hwan513 <100452681+hwan513@users.noreply.github.com>
2023-02-15 12:50:27 +1300

    feat: comment out ngram option in ltex lsp

--- .config/nvim/lua/john/lsp/settings/ltex.lua
+++ .config/nvim/lua/john/lsp/settings/ltex.lua
@@ -2,10 +2,10 @@ return {
   -- install ngrams from https://dev.languagetool.org/finding-errors-using-n-gram-data.html
   -- save the folder at ~/ngrams/en/
   settings = {
-    ltex = {
-      additionalRules = {
-        languageModel = "~/ngrams/",
-      },
-    },
+    -- ltex = {
+    --   additionalRules = {
+    --     languageModel = "~/ngrams/",
+    --   },
+    -- },
   },
 }
