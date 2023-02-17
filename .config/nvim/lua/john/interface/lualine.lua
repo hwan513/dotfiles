fcf5f69a729b459836744cf91d3bac5d9b35bc12
hwan513 <100452681+hwan513@users.noreply.github.com>
2023-02-18 09:41:47 +1300

    feat: extract icons into separate file, modify look of lualine

--- .config/nvim/lua/john/interface/lualine.lua
+++ .config/nvim/lua/john/interface/lualine.lua
@@ -1,14 +1,15 @@
 local status_ok, lualine = pcall(require, "lualine")
-
 if not status_ok then
   return
 end
+
+local icons = require("john.misc.icons")
 
 local diagnostics = {
   "diagnostics",
   sources = { "nvim_diagnostic" },
   sections = { "error", "warn" },
-  symbols = { error = " ", warn = " " },
+  symbols = { error = icons.diagnostics.Error, warn = icons.diagnostics.Warn },
   colored = true,
   update_in_insert = false,
   always_visible = true,
@@ -23,57 +24,66 @@ local mode = {
   cond = nil,
 }
 
-local progress = {
-  function()
-    local current_line = vim.fn.line(".")
-    local total_lines = vim.fn.line("$")
-    local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-    local line_ratio = current_line / total_lines
-    local index = math.ceil(line_ratio * #chars)
-    return chars[index]
-  end,
-  padding = { left = 0, right = 0 },
-  cond = nil,
-  color = { fg = "#F7768E", bg = "#16161E" },
-}
+-- local progress = {
+--   function()
+--     local current_line = vim.fn.line(".")
+--     local total_lines = vim.fn.line("$")
+--     local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+--     local line_ratio = current_line / total_lines
+--     local index = math.ceil(line_ratio * #chars)
+--     return chars[index]
+--   end,
+--   padding = { left = 0, right = 0 },
+--   cond = nil,
+--   color = { fg = "#F7768E", bg = "#16161E" },
+-- }
 
 local filename = {
   "filename",
   file_status = true, -- Displays file status (readonly status, modified status)
-  path = 0, -- 0: Just the filename
+  path = 1,
+  -- 0: Just the filename
   -- 1: Relative path
   -- 2: Absolute path
 
   shorting_target = 40, -- Shortens path to leave 40 spaces in the window
   -- for other components. (terrible name, any suggestions?)
-  symbols = {
-    modified = "[+]", -- Text to show when the file is modified.
-    readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-    unnamed = "[ ]", -- Text to show for unnamed buffers.
-  },
+  symbols = { modified = "  ", readonly = "", unnamed = "" },
 }
 
--- local location = {
--- 	"location",
--- 	padding = { left = 1, right = 1 },
--- }
+local location = {
+  "location",
+  padding = { left = 1, right = 1 },
+}
+
+local diff = {
+  "diff",
+  colored = true, -- Displays a colored diff status if set to true
+  symbols = {
+    added = icons.git.added,
+    modified = icons.git.modified,
+    removed = icons.git.removed,
+  },
+}
 
 lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "NvimTree", "Mundo", "MundoDiff", "toggleterm" },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    -- component_separators = { left = "", right = "" },
+    -- section_separators = { left = "", right = "" },
+    disabled_filetypes = { "NvimTree", "toggleterm" },
     always_divide_middle = true,
   },
   sections = {
     lualine_a = { mode },
-    lualine_b = { "branch", "diff" },
+    lualine_b = { "branch", diff, diagnostics },
     lualine_c = { filename },
-    lualine_x = { "filetype" },
-    lualine_y = { diagnostics },
-    lualine_z = { progress },
+    lualine_x = { "encoding", "filetype" },
+    lualine_y = { location },
+    lualine_z = { "progress" },
   },
   inactive_sections = {
     lualine_a = {},
--- .config/nvim/lua/john/lsp/handlers.lua
+++ .config/nvim/lua/john/lsp/handlers.lua
@@ -1,11 +1,13 @@
 local M = {}
 
+local icons = require("john.misc.icons")
+
 M.setup = function()
   local signs = {
-    { name = "DiagnosticSignError", text = " " },
-    { name = "DiagnosticSignWarn",  text = " " },
-    { name = "DiagnosticSignHint",  text = " " },
-    { name = "DiagnosticSignInfo",  text = " " },
+    { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+    { name = "DiagnosticSignWarn",  text = icons.diagnostics.Warn },
+    { name = "DiagnosticSignHint",  text = icons.diagnostics.Hint },
+    { name = "DiagnosticSignInfo",  text = icons.diagnostics.Info },
   }
 
   for _, sign in ipairs(signs) do
