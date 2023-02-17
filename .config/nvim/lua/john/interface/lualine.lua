12cd92096ad506e23e5f5f07a914d14f8b565dc3
hwan513 <100452681+hwan513@users.noreply.github.com>
2023-02-18 12:34:16 +1300

    feat: ignore neogit filetype for lualine

--- .config/nvim/lua/john/interface/lualine.lua
+++ .config/nvim/lua/john/interface/lualine.lua
@@ -74,7 +74,7 @@ lualine.setup({
     section_separators = { left = '', right = '' },
     -- component_separators = { left = "", right = "" },
     -- section_separators = { left = "", right = "" },
-    disabled_filetypes = { "NvimTree", "toggleterm" },
+    disabled_filetypes = { "NvimTree", "toggleterm", "NeogitStatus" },
     always_divide_middle = true,
   },
   sections = {
