147d9a2502afef1b795ef10647f2bbd960736947
hwan513 <100452681+hwan513@users.noreply.github.com>
2023-02-18 16:33:40 +1300

    fix: text files don't set spell only wrap

--- .config/nvim/lua/john/default/autocommands.lua
+++ .config/nvim/lua/john/default/autocommands.lua
@@ -38,12 +38,12 @@ autocmd({ "FocusGained", "BufEnter" }, {
 })
 
 -- Enable spell checking for certain file types and wrapping
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.txt", "*.md", "*.tex" },
   callback = function()
     vim.api.nvim_exec(
       [[
-    setlocal spell
+    " setlocal spell
     set wrap
     ]],
       { output = false }
