80a8593cfa7495fe6812e06356d54bc0fc3a1f35
hwan513 <100452681+hwan513@users.noreply.github.com>
2023-03-16 16:51:15 +1300

    style: can't tell if any new plugins have been added but change formatting

--- .config/nvim/lua/john/lazy.lua
+++ .config/nvim/lua/john/lazy.lua
@@ -4,11 +4,15 @@ local req = function(modname) return function() require(modname) end end
 local setup = function(modname, args) return function() require(modname).setup(args) end end
 
 local plugins = {
-  { "nvim-lua/plenary.nvim",    lazy = true }, -- Useful lua functions used ny lots of plugins
-  { "dstein64/vim-startuptime", cmd = { "StartupTime" }, }, -- startup timer,
+  { "nvim-lua/plenary.nvim",    lazy = true },                                           -- Useful lua functions used ny lots of plugins
+  { "dstein64/vim-startuptime", cmd = { "StartupTime" }, },                              -- startup timer,
   { "folke/which-key.nvim",     event = "VeryLazy",      config = setup("which-key"), }, -- need to finish which-key config
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000,
-    config = function() vim.cmd([[colorscheme tokyonight-night]]) end, }, -- colourscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function() vim.cmd([[colorscheme tokyonight-night]]) end,
+  }, -- colourscheme
   { "catppuccin/nvim",        name = "catppuccin", event = "VeryLazy" },
 
 
@@ -16,32 +20,35 @@ local plugins = {
   { "machakann/vim-sandwich", event = "VeryLazy" }, -- surrounding stuff with stuff
 
   -- treesitter
-  { "nvim-treesitter/nvim-treesitter",
-    config = req("john.treesitter.treesitter"), build = ":TSUpdate", event = { "VeryLazy" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = req("john.treesitter.treesitter"),
+    build = ":TSUpdate",
+    event = { "VeryLazy" },
     dependencies = {
       { "lukas-reineke/indent-blankline.nvim", config = req("john.interface.indentline") }, -- shows indent level with line
-      { "windwp/nvim-autopairs",               config = req("john.qol.autopairs") }, -- autopair brackets and quotations
-      "windwp/nvim-ts-autotag", -- automatically add tags to end of documents
+      { "windwp/nvim-autopairs",               config = req("john.qol.autopairs") },        -- autopair brackets and quotations
+      "windwp/nvim-ts-autotag",                                                             -- automatically add tags to end of documents
       "JoosepAlviste/nvim-ts-context-commentstring",
-      "p00f/nvim-ts-rainbow", -- bracket pair matching
+      "p00f/nvim-ts-rainbow",                                                               -- bracket pair matching
       "nvim-treesitter/nvim-treesitter-textobjects",
       { "RRethy/nvim-treesitter-textsubjects", config = req("john.treesitter.textsubjects") },
       { "RRethy/vim-illuminate",               config = req("john.qol.illuminate") }, -- highlights current thing under cursor
     }
-
-  }, -- better syntax highlighting and other stuff
+  },                                                                                  -- better syntax highlighting and other stuff
 
   -- user interface {{{
   { "nvim-tree/nvim-web-devicons", lazy = true },
   { "nvim-tree/nvim-tree.lua",     keys = "<C-e>",                         config = req("john.interface.nvim-tree") },
   { "akinsho/bufferline.nvim",     event = "VeryLazy",                     config = req("john.interface.bufferline") },
-  { "famiu/bufdelete.nvim",        cmd = "Bdelete" }, -- delete buffers nicely
+  { "famiu/bufdelete.nvim",        cmd = "Bdelete" },                                                                -- delete buffers nicely
   { "nvim-lualine/lualine.nvim",   event = "VeryLazy",                     config = req("john.interface.lualine") }, -- status line plugin
-  { "norcalli/nvim-colorizer.lua", event = { "BufNewFile", "BufReadPre" }, config = setup("colorizer") }, -- coloring viewer for html css stuff
+  { "norcalli/nvim-colorizer.lua", event = { "BufNewFile", "BufReadPre" }, config = setup("colorizer") },            -- coloring viewer for html css stuff
 
   -- }}}
   -- completion and snippets
-  { "hrsh7th/nvim-cmp",
+  {
+    "hrsh7th/nvim-cmp",
     config = function()
       require("john.qol.cmp")
       require("john.lsp.handlers").setup()
@@ -49,23 +56,27 @@ local plugins = {
     -- config = req("john.qol.cmp"),
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
-      "hrsh7th/cmp-buffer", -- buffer completions
-      "hrsh7th/cmp-path", -- path completions
-      "hrsh7th/cmp-cmdline", -- cmdline completions
-      "saadparwaiz1/cmp_luasnip", -- snippet completions
-      "hrsh7th/cmp-nvim-lsp", -- lsp completions
-      "hrsh7th/cmp-nvim-lua", -- lua completions for nvim config
-      "andersevenrud/cmp-tmux", -- tmux completions
-      "L3MON4D3/LuaSnip", --snippet engine
+      "hrsh7th/cmp-buffer",           -- buffer completions
+      "hrsh7th/cmp-path",             -- path completions
+      "hrsh7th/cmp-cmdline",          -- cmdline completions
+      "saadparwaiz1/cmp_luasnip",     -- snippet completions
+      "hrsh7th/cmp-nvim-lsp",         -- lsp completions
+      "hrsh7th/cmp-nvim-lua",         -- lua completions for nvim config
+      "andersevenrud/cmp-tmux",       -- tmux completions
+      "L3MON4D3/LuaSnip",             --snippet engine
       "rafamadriz/friendly-snippets", -- a bunch of snippets to use
     }
-  }, -- The completion plugin
+  },                                  -- The completion plugin
 
   -- language server protocol {{{
-  { "folke/noice.nvim", config = req("john.interface.noice"),
+  {
+    "folke/noice.nvim",
+    config = req("john.interface.noice"),
     dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify", },
-    event = "VeryLazy" },
-  { "neovim/nvim-lspconfig",
+    event = "VeryLazy"
+  },
+  {
+    "neovim/nvim-lspconfig",
     config = req("lspconfig"),
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
@@ -81,7 +92,23 @@ local plugins = {
     }
   }, -- enable LSP
 
+
+  -- Markdown Editing
+  {
+    "abeleinin/papyrus",
+    config = function()
+      vim.cmd([[
+        let g:papyrus_latex_engine = 'pdflatex'
+        let g:papyrus_viewer = 'preview'
+      ]])
+    end,
+    dependencies = { "vim-pandoc/vim-pandoc-syntax" },
+    cmd = { "PapyrusAutoCompile" },
+  },
+
   -- {"davidgranstrom/nvim-markdown-preview"} -- alternative preview plugin
+
+
 
   -- -- debug adaptor protocol
   -- -- one day I'll work more on lazy loading eveything
@@ -97,9 +124,14 @@ local plugins = {
 
   -- telescope
   { "ahmedkhalf/project.nvim", event = "VeryLazy",                     config = setup("project_nvim"), }, -- project managing
-  { "AckslD/nvim-neoclip.lua", event = "VeryLazy", config = setup("neoclip", { enable_persistent_history = true, }),
-    dependencies = { 'kkharji/sqlite.lua' }, },
-  { "nvim-telescope/telescope.nvim",
+  {
+    "AckslD/nvim-neoclip.lua",
+    event = "VeryLazy",
+    config = setup("neoclip", { enable_persistent_history = true, }),
+    dependencies = { 'kkharji/sqlite.lua' },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
     config = setup("john.interface.telescope"),
     keys = require("john.interface.telescope").keys,
     cmd = "Telescope",
@@ -113,7 +145,9 @@ local plugins = {
   -- git stuff
   { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, config = req("john.interface.gitsigns") },
   { "sindrets/diffview.nvim",  cmd = "DiffviewOpen" },
-  { "TimUntersberger/neogit", cmd = "Neogit",
+  {
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
     config = setup("neogit", { integrations = { diffview = true }, disable_commit_confirmation = true }),
   },
 
@@ -122,12 +156,18 @@ local plugins = {
   { "numToStr/Comment.nvim",   keys = { "gcc", { "gc", mode = "v" } },  config = req("john.qol.comment") },
   { "gbprod/cutlass.nvim",     keys = { { "m", mode = { "v", "n" } } }, config = setup("cutlass", { cut_key = "m", }) },
   { "folke/persistence.nvim",  event = "BufReadPre",                    config = setup("persistence"), }, -- session manager
-  { "akinsho/toggleterm.nvim", version = "*", keys = "<C-\\>",
-    config = req("john.interface.terminal") }, -- toggle nvim terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = "<C-\\>",
+    config = req("john.interface.terminal")
+  },                                            -- toggle nvim terminal
   { "tpope/vim-abolish",  event = "VeryLazy" }, -- case coersion, substition, abbreviation
   { "github/copilot.vim", cmd = "Copilot",   config = req("john.qol.copilot") },
-  { "ggandor/leap.nvim",
-    dependencies = { { "ggandor/flit.nvim", config = setup("flit") }, } },
+  {
+    "ggandor/leap.nvim",
+    dependencies = { { "ggandor/flit.nvim", config = setup("flit") }, }
+  },
 
 
   -- I have the funny
