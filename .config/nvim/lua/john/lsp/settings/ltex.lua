b8846048805415e959c6b5cccf05eac793effa47
Henry Wang <henrywang@wifi-172-23-108-66.uoa-wifi.auckland.ac.nz>
2023-05-23 15:07:28 +1200

    feat: change configs for ltex lsp

--- .config/nvim/lua/john/lsp/settings/ltex.lua
+++ .config/nvim/lua/john/lsp/settings/ltex.lua
@@ -1,11 +1,26 @@
+local path = vim.fn.stdpath 'config' .. '/spell/en.utf-8.add'
+local words = {}
+
+for word in io.open(path, 'r'):lines() do
+  table.insert(words, word)
+end
+
 return {
-  -- install ngrams from https://dev.languagetool.org/finding-errors-using-n-gram-data.html
-  -- save the folder at ~/ngrams/en/
   settings = {
-    -- ltex = {
-    --   additionalRules = {
-    --     languageModel = "~/ngrams/",
-    --   },
-    -- },
+    ltex = {
+      -- install ngrams from https://dev.languagetool.org/finding-errors-using-n-gram-data.html
+      -- save the folder at ~/ngrams/en/
+      --   additionalRules = {
+      --     languageModel = "~/ngrams/",
+      --   },
+      disabledRules = {
+        ['en-US'] = { 'PROFANITY' },
+        ['en-GB'] = { 'PROFANITY' },
+      },
+      dictionary = {
+        ['en-US'] = words,
+        ['en-GB'] = words,
+      },
+    },
   },
 }
