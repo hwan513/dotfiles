ff1cfe756bc6a303b39aaa69ce77182e1904474e
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-03-13 20:07:13 +1300

    feat: add more games to neovim

--- .config/nvim/lua/john/lazy.lua
+++ .config/nvim/lua/john/lazy.lua
@@ -191,8 +191,12 @@ local plugins = {
   { "hsanson/vim-android" },
 
   -- I have the funny
-  { "alec-gibson/nvim-tetris",      cmd = "Tetris" },
-  { "ryoppippi/bad-apple.vim",      cmd = "BadApple", dependencies = "vim-denops/denops.vim", },
+  { "alec-gibson/nvim-tetris",        cmd = "Tetris" },
+  { "seandewar/nvimesweeper",         cmd = "Nvimesweeper" },
+  { "seandewar/killersheep.nvim",     cmd = "KillKillKill" },
+  { "rktjmp/shenzhen-solitaire.nvim", cmd = "ShenzhenSolitaireNewGame" },
+  { "alanfortlink/blackjack.nvim",    cmd = "BlackJackNewGame" },
+  { "ryoppippi/bad-apple.vim",        cmd = "BadApple",                dependencies = "vim-denops/denops.vim", },
   { dir = "~/projects/typing-test", },
 }
 
