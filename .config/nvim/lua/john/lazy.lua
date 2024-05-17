0d36b77c2390656d58a31bee81ba6c3c5b77bf8f
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-05-17 23:40:06 +1200

    chore: update plugins

--- .config/nvim/lua/john/lazy.lua
+++ .config/nvim/lua/john/lazy.lua
@@ -78,7 +78,7 @@ local plugins = {
   -- user interface {{{
   { "nvim-tree/nvim-web-devicons", lazy = true },
   { "nvim-tree/nvim-tree.lua",     keys = "<C-e>",                         config = req("john.interface.nvim-tree") },
-  { "akinsho/bufferline.nvim",     event = "VeryLazy",                     commit = "f6f00d9ac1a51483ac78418f9e63126119a70709", config = req("john.interface.bufferline") },
+  { "akinsho/bufferline.nvim",     event = "VeryLazy",                     config = req("john.interface.bufferline") },
   { "famiu/bufdelete.nvim",        cmd = "Bdelete" },                                                                -- delete buffers nicely
   { "nvim-lualine/lualine.nvim",   event = "VeryLazy",                     config = req("john.interface.lualine") }, -- status line plugin
   { "norcalli/nvim-colorizer.lua", event = { "BufNewFile", "BufReadPre" }, config = setup("colorizer") },            -- coloring viewer for html css stuff
@@ -217,6 +217,7 @@ local plugins = {
     "TimUntersberger/neogit",
     cmd = "Neogit",
     branch = "nightly",
+    commit = "20ab5a688c2de6fa371865528c737ae901989aa5",
     config = setup("neogit", { integrations = { diffview = true, telescope = true } }),
   },
 
