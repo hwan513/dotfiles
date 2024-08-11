aab8234f4c8315863e8abfb51826b73e486ba746
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-08-11 16:29:32 +1200

    feat(nvim): setup bufferline config

--- .config/nvim/lua/john/ui/init.lua
+++ .config/nvim/lua/john/ui/init.lua
@@ -1,5 +1,4 @@
 local prequire = require("john.utils").prequire
-local setup = require("john.utils").setup
 local req = require("john.utils").req
 
 return {
@@ -8,7 +7,7 @@ return {
   prequire("john.ui.mini-icons"),
   prequire("john.ui.mini-bufremove"),
   prequire("john.ui.mini-hipatterns"),
+  prequire("john.ui.bufferline"),
   { "stevearc/oil.nvim", opts = { view_options = { show_hidden = true } } },
-  { "akinsho/bufferline.nvim", event = "VeryLazy", config = req("john.interface.bufferline") },
   { "nvim-lualine/lualine.nvim", event = "VeryLazy", config = req("john.interface.lualine") }, -- status line plugin
 }
