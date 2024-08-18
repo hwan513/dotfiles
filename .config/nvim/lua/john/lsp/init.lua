744fc1b15c759534cdf5b3abad2bb35f32a1c3f6
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-08-18 21:14:13 +1200

    feat: add luassert and busted types into neovim

--- .config/nvim/lua/john/lsp/init.lua
+++ .config/nvim/lua/john/lsp/init.lua
@@ -1,18 +1,22 @@
 local req = require("john.utils").req
 local setup = require("john.utils").setup
 return {
-  -- {
-  --   "folke/lazydev.nvim",
-  --   ft = "lua", -- only load on lua files
-  --   opts = {
-  --     library = {
-  --       -- See the configuration section for more details
-  --       -- Load luvit types when the `vim.uv` word is found
-  --       { path = "luvit-meta/library", words = { "vim%.uv" } },
-  --     },
-  --   },
-  -- },
-  -- { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  { "LuaCATS/luassert", name = "luassert-types", lazy = true },
+  { "LuaCATS/busted", name = "busted-types", lazy = true },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "luassert-types/library", words = { "assert" } },
+        { path = "busted-types/library", words = { "describe" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
   { "folke/neoconf.nvim", opts = {} },
   {
     "neovim/nvim-lspconfig",
