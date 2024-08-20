03406fbe36adae2bdd9842d73b701fb4954792ee
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-08-20 15:15:03 +1200

    feat: add octo.nvim

--- .config/nvim/lua/john/git/init.lua
+++ .config/nvim/lua/john/git/init.lua
@@ -3,4 +3,5 @@ return {
   prequire("john.git.neogit"),
   prequire("john.git.gitsigns"),
   { "sindrets/diffview.nvim", cmd = "DiffviewOpen" },
+  { "pwntester/octo.nvim", opts = {}, event = "VeryLazy" },
 }
