31ca2acb0701de87d96a9d01ccb3be5037f5ea2c
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-08-23 22:48:52 +1200

    fix(nvim): pin dropbar to commit due to bug

--- .config/nvim/lua/john/editor/dropbar.lua
+++ .config/nvim/lua/john/editor/dropbar.lua
@@ -3,4 +3,5 @@ return {
   dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
   event = "LspAttach",
   keys = { { "gp", "<cmd>lua require('dropbar.api').pick()<cr>", desc = "Focus Lsp Context" } },
+  commit = "d26bf92161cd70e049dc138b44ffa0246dbf7178",
 }
