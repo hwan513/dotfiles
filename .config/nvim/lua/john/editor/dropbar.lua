2053e3acc1f157684c05b3e70beb50ab67effc38
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-08-28 21:07:49 +1200

    fix(nvim): unpin dropbar as issue is fixed

--- .config/nvim/lua/john/editor/dropbar.lua
+++ .config/nvim/lua/john/editor/dropbar.lua
@@ -2,6 +2,5 @@ return {
   "Bekaboo/dropbar.nvim",
   dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
   event = "LspAttach",
-  keys = { { "gp", "<cmd>lua require('dropbar.api').pick()<cr>", desc = "Focus Lsp Context" } },
-  commit = "d26bf92161cd70e049dc138b44ffa0246dbf7178",
+  keys = { { "gp", "<cmd>lua require('dropbar.api').pick()<cr>", desc = "Focus Dropbar" } },
 }
