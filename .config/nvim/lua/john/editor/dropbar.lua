1c9a4cd4fd2ae69bd53655e89e8f666e34cca958
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-10-11 12:26:48 +1300

    fix(nvim): dropbar lazy loading issue with autosession

--- .config/nvim/lua/john/editor/dropbar.lua
+++ .config/nvim/lua/john/editor/dropbar.lua
@@ -2,5 +2,6 @@ return {
   "Bekaboo/dropbar.nvim",
   dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
   event = "LspAttach",
+  lazy = false,
   keys = { { "gp", "<cmd>lua require('dropbar.api').pick()<cr>", desc = "Focus Dropbar" } },
 }
