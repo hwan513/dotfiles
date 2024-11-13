d19269156d4e488c9cb8f9d669c2ea14723696a1
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-11-13 17:22:30 +1300

    fix(hammerspoon): don't reload when applying cache update

--- .hammerspoon/reloadConfig.lua
+++ .hammerspoon/reloadConfig.lua
@@ -1,6 +1,11 @@
-hs.alert.show("Config loaded")
-local function reloadConfig()
-  hs.reload()
+function ReloadConfig(paths)
+  for _, path in ipairs(paths) do
+    if path:sub(-4) == ".lua" then
+      hs.reload()
+    end
+  end
 end
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", reloadConfig)
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "R", hs.reload)
+ReloadWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", ReloadConfig):start()
+hs.alert.show("Config reloaded")
