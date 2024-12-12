5166ba1e22d96ebe16599cb1701df6ee6d6da22c
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-12-12 18:53:13 +1300

    feat(nvim): add back lsp settings for lua_ls

---
+++ .config/nvim/lua/john/lsp/settings/lua_ls.lua
@@ -0,0 +1,20 @@
+return {
+  settings = {
+    Lua = {
+      codeLens = {
+        enable = true,
+      },
+      completion = {
+        callSnippet = "Replace",
+      },
+      hint = {
+        enable = true,
+        setType = false,
+        paramType = true,
+        paramName = "Disable",
+        semicolon = "Disable",
+        arrayIndex = "Disable",
+      },
+    },
+  },
+}
