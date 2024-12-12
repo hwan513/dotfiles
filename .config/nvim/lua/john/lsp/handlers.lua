d324f255d4264abab1baca78f707ef92386c9f6e
Henry Wang <hwan513@aucklanduni.ac.nz>
2024-12-13 00:39:25 +1300

    feat(nvim): add codelens and inlay hint keymaps

--- .config/nvim/lua/john/lsp/handlers.lua
+++ .config/nvim/lua/john/lsp/handlers.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 local icons = require("john.misc.icons")
 
 M.setup = function()
@@ -23,31 +22,12 @@ M.setup = function()
     },
   })
 
-  vim.lsp.inlay_hints = {
-    enabled = true,
-  }
-  vim.api.nvim_create_user_command("ToggleInlay", function()
-    local inlay_hint = vim.lsp.inlay_hint
-    inlay_hint.enable(not inlay_hint.is_enabled())
-  end, { nargs = 0 })
-
   -- Remove default lsp keymaps
   vim.keymap.del("n", "grn") -- Rename
   vim.keymap.del({ "n", "x" }, "gra") -- Code Action
   vim.keymap.del("n", "grr") -- References
-
-  -- Register keymaps
-  vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local buffer = args.buf ---@type number
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      require("john.lsp.keymaps")(buffer, client)
-    end,
-  })
-end
-
-M.on_attach = function(client, buffer)
-  -- require("john.lsp.keymaps")(buffer)
+  -- Setup lsp keymaps
+  require("john.lsp.keymaps")
 end
 
 M.capabilities = function()
--- .config/nvim/lua/john/lsp/keymaps.lua
+++ .config/nvim/lua/john/lsp/keymaps.lua
@@ -1,31 +1,42 @@
+-- stylua: ignore start
 local keys = {
-  { "n", "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "textDocument/definition" },
-  { "n", "gD", vim.lsp.buf.declaration, desc = "Goto Declaration", has = "textDocument/declaration" },
-  { "n", "gI", vim.lsp.buf.implementation, desc = "Goto Implementation", has = "textDocument/implementation" },
-  { "n", "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition", has = "textDocument/typeDefinition" },
-  { "n", "K", vim.lsp.buf.hover, desc = "Hover", has = "textDocument/hover" },
-  { "n", "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "textDocument/signatureHelp" },
-  { "i", "<C-s>", vim.lsp.buf.signature_help, desc = "Signature Help", has = "textDocument/signatureHelp" },
-  { { "n", "v" }, "ga", vim.lsp.buf.code_action, desc = "Code Action", has = "textDocument/codeAction" },
-  { "n", "gn", vim.lsp.buf.rename, desc = "Rename", has = "textDocument/rename" },
-  { "n", "gr", vim.lsp.buf.references, desc = "References", has = "textDocument/references" },
+  { "n", "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
+  { "n", "gD", vim.lsp.buf.declaration, desc = "Goto Declaration", has = "declaration" },
+  { "n", "gI", vim.lsp.buf.implementation, desc = "Goto Implementation", has = "implementation" },
+  { "n", "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition", has = "typeDefinition" },
+  { "n", "K", vim.lsp.buf.hover, desc = "Hover", has = "hover" },
+  { "n", "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+  { "i", "<C-s>", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+  { { "n", "x" }, "ga", vim.lsp.buf.code_action, desc = "Code Action", has = "codeAction" },
+  { { "n", "x" }, "gl", vim.lsp.codelens.run, desc = "Run Codelens", has = "codeLens" },
+  { "n", "gL", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", has = "codeLens" },
+  { "n", "gh", "<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>", desc = "Toggle Inlay Hints", has = "inlayHint" },
+  { "n", "gn", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
+  { "n", "gr", vim.lsp.buf.references, desc = "References", has = "references" },
   { "n", "<C-p>", "<cmd>lua vim.diagnostic.goto_prev({ border = 'rounded' })<CR>", desc = "Next Diagnostic" },
   { "n", "<C-n>", "<cmd>lua vim.diagnostic.goto_next({ border = 'rounded' })<CR>", desc = "Previous Diagnostic" },
 }
+-- stylua: ignore end
 
--- local client2 = vim.lsp.get_clients()[1]
--- -- local supports = client2.supports_method("textDocument/inlayHint")
--- local supports = client2.supports_method("textDocument/codeLens")
--- print(supports)
--- vim.lsp.codelens.refresh()
--- vim.lsp.codelens.get(0)
-
-return function(buffer, client)
+--- @param buffer number
+--- @param client vim.lsp.Client
+local function setup(buffer, client)
   local map = require("john.utils").map
   for _, key in ipairs(keys) do
     local mode, mapping, fn = unpack(key)
-    if not key.has or client.supports_method(key.has) then
+    if not key.has or client:supports_method("textDocument/" .. key.has) then
       map(mode, mapping, fn, { noremap = true, silent = true, buffer = buffer, desc = key.desc })
     end
   end
 end
+
+-- Register keymap autocommand
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local buffer = args.buf ---@type number
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client ~= nil then
+      setup(buffer, client)
+    end
+  end,
+})
