3d68c29c16584606c84ff06db9ad29446d8563f2
Henry Wang <wang.henryhan@gmail.com>
2025-02-18 11:23:01 +1300

    feat(nvim): setup conform settings for css and rust

--- .config/nvim/lua/john/lsp/conform.lua
+++ .config/nvim/lua/john/lsp/conform.lua
@@ -8,8 +8,10 @@ return {
       python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
       javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
       typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+      css = { "biome", "prettierd", "prettier", stop_after_first = true },
       json = { "prettierd", "prettier", stop_after_first = true },
       html = { "prettierd", "prettier", stop_after_first = true },
+      rust = { "leptosfmt", "rustfmt" },
     },
     format_on_save = {
       -- These options will be passed to conform.format()
