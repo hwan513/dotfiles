ad7373c0ca40ba1b7b42dae033c81ec9d58c0c2e
Henry Wang <wang.henryhan@gmail.com>
2025-02-18 11:21:00 +1300

    feat(kanata): comment out kanata tray installation

--- .config/kanata/install.sh
+++ .config/kanata/install.sh
@@ -10,10 +10,10 @@ rm Karabiner*.pkg
 # cargo binstall kanata
 
 # Install kanata tray
-gh release download -R "https://github.com/rszyma/kanata-tray.git" --pattern "*macos"
-sudo chmod +x kanata-tray-macos
-sudo chmod +x "$XDG_CONFIG_HOME/kanata/KanataTray/KanataTray"
-mkdir -p "/Applications/KanataTray.app/Contents/MacOS/"
-sudo cp -r "$XDG_CONFIG_HOME/kanata/KanataTray/" "/Applications/KanataTray.app/Contents/MacOS/"
-mv kanata-tray-macos "/Applications/KanataTray.app/Contents/MacOS/kanata-tray-macos"
-cp "$XDG_CONFIG_HOME/kanata/Info.plist" "/Applications/KanataTray.app/Contents/"
+# gh release download -R "https://github.com/rszyma/kanata-tray.git" --pattern "*macos"
+# sudo chmod +x kanata-tray-macos
+# sudo chmod +x "$XDG_CONFIG_HOME/kanata/KanataTray/KanataTray"
+# mkdir -p "/Applications/KanataTray.app/Contents/MacOS/"
+# sudo cp -r "$XDG_CONFIG_HOME/kanata/KanataTray/" "/Applications/KanataTray.app/Contents/MacOS/"
+# mv kanata-tray-macos "/Applications/KanataTray.app/Contents/MacOS/kanata-tray-macos"
+# cp "$XDG_CONFIG_HOME/kanata/Info.plist" "/Applications/KanataTray.app/Contents/"
