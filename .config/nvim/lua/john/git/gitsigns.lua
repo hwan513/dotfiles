a5f8c72072b0cd544d4aac9c153a72d5d919065c
Henry Wang <wang.henryhan@gmail.com>
2025-04-25 10:14:13 +1200

    feat(nvim): update gitsigns mappings

--- .config/nvim/lua/john/git/gitsigns.lua
+++ .config/nvim/lua/john/git/gitsigns.lua
@@ -32,14 +32,13 @@ return {
       map("v", "<leader>gs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Stage Hunk")
       map("v", "<leader>gr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end, "Reset Hunk")
       map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
-      map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+      map("n", "<leader>gp", gs.preview_hunk_inline, "Preview Hunk Inline")
+      map("n", "<leader>gP", gs.preview_hunk, "Preview Hunk")
       map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
       map("n", "<leader>gB", gs.toggle_current_line_blame)
       map("n", "<leader>gd", gs.diffthis)
       map("n", "<leader>gD", function() gs.diffthis("~") end)
-      map("n", "<leader>gt", gs.toggle_deleted)
       -- stylua: ignore end
     end,
   },
