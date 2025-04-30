a9715732d8885463a30ab2ff9ec61b7e49d1b9db
Henry Wang <wang.henryhan@gmail.com>
2025-04-30 16:13:31 +1200

    refactor(nvim): replace smart splits keymaps definition with anonymous functions

--- .config/nvim/lua/john/coding/smart-splits.lua
+++ .config/nvim/lua/john/coding/smart-splits.lua
@@ -3,14 +3,16 @@ return {
   lazy = false,
   opts = {},
   keys = {
-    { "<C-h>", "<cmd> lua require('smart-splits').move_cursor_left()<cr>", desc = "Move cursor left" },
-    { "<C-j>", "<cmd> lua require('smart-splits').move_cursor_down()<cr>", desc = "Move cursor down" },
-    { "<C-k>", "<cmd> lua require('smart-splits').move_cursor_up()<cr>", desc = "Move cursor up" },
-    { "<C-l>", "<cmd> lua require('smart-splits').move_cursor_right()<cr>", desc = "Move cursor right" },
-    { "<C-BS>", "<cmd> lua require('smart-splits').move_cursor_previous()<cr>", desc = "Move cursor previous" },
-    { "<A-h>", "<cmd> lua require('smart-splits').resize_left(5)<cr>", desc = "Resize left" },
-    { "<A-j>", "<cmd> lua require('smart-splits').resize_down(5)<cr>", desc = "Resize down" },
-    { "<A-k>", "<cmd> lua require('smart-splits').resize_up(5)<cr>", desc = "Resize up" },
-    { "<A-l>", "<cmd> lua require('smart-splits').resize_right(5)<cr>", desc = "Resize right" },
+    -- stylua: ignore start
+    { "<C-h>", function() require('smart-splits').move_cursor_left() end, desc = "Move cursor left" },
+    { "<C-j>", function() require('smart-splits').move_cursor_down() end, desc = "Move cursor down" },
+    { "<C-k>", function() require('smart-splits').move_cursor_up() end, desc = "Move cursor up" },
+    { "<C-l>", function() require('smart-splits').move_cursor_right() end, desc = "Move cursor right" },
+    { "<C-BS>", function() require('smart-splits').move_cursor_previous() end, desc = "Move cursor previous" },
+    { "<A-h>", function() require('smart-splits').resize_left(5) end, desc = "Resize left" },
+    { "<A-j>", function() require('smart-splits').resize_down(5) end, desc = "Resize down" },
+    { "<A-k>", function() require('smart-splits').resize_up(5) end, desc = "Resize up" },
+    { "<A-l>", function() require('smart-splits').resize_right(5) end, desc = "Resize right" },
+    -- stylua: ignore end
   },
 }
