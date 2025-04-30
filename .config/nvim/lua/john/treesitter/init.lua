082b8c1a6c42fb73dd0af44a6a3d8c7af9f31c51
Henry Wang <wang.henryhan@gmail.com>
2025-05-01 10:47:44 +1200

    fix(nvim): remove unneeded setup used for rainbow-delimiters

--- .config/nvim/lua/john/treesitter/init.lua
+++ .config/nvim/lua/john/treesitter/init.lua
@@ -2,5 +2,5 @@ local prequire = require("john.utils").prequire
 return {
   { "rayliwell/tree-sitter-rstml", opts = {}, ft = "rust" },
   prequire("john.treesitter.treesitter"),
-  { "HiPhish/rainbow-delimiters.nvim", opts = {} },
+  { "HiPhish/rainbow-delimiters.nvim" },
 }
