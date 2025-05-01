5e82bbae8ee8d1e924e58ec0d33a88dc87c78592
Henry Wang <wang.henryhan@gmail.com>
2025-05-01 16:11:12 +1200

    refactor(nvim): remove unused noice setup code

--- .config/nvim/lua/john/interface/noice.lua
+++ .config/nvim/lua/john/interface/noice.lua
@@ -2,64 +2,19 @@ return {
   "folke/noice.nvim",
   event = "VeryLazy",
   -- does require snacks.nvim to be installed, defined elsewhere
-  init = function()
-    local status_ok, telescope = pcall(require, "telescope")
-    if status_ok then
-      telescope.load_extension("noice")
-    end
-  end,
   opts = {
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
       },
     },
-    messages = {
-      max_width = 10,
-    },
-    views = {
-      notify = {
-        border = {
-          style = "none",
-          -- style = { '┌', '─', '┐', '│', '┘', '─', '└', '│' },
-          padding = { 1, 3 },
-        },
-        -- win_options = { winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder', },
-        position = { row = 14, col = "50%" },
-      },
-    },
-
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-    },
-    routes = {
-      {
-        filter = {
-          event = "notify",
-          min_width = 60,
-        },
-        view = "split",
-      },
-      {
-        filter = {
-          event = "messages",
-          min_width = 60,
-        },
-        view = "split",
-      },
-      {
-        filter = {
-          event = "Error",
-          min_width = 60,
-        },
-        view = "split",
-      },
     },
   },
 }
