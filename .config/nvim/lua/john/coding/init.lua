9332f97ac93f4d176a937dcaa4fbaf9d979ee1d7
Henry Wang <wang.henryhan@gmail.com>
2025-05-02 22:03:26 +1200

    style(nvim): reformat supermaven definition

--- .config/nvim/lua/john/coding/init.lua
+++ .config/nvim/lua/john/coding/init.lua
@@ -3,6 +3,11 @@ return {
   prequire("john.coding.autopairs"),
   prequire("john.coding.blink"),
   prequire("john.coding.markup"),
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = { keymaps = { accept_suggestion = "<M-j>", clear_suggestion = "<C-]>", accept_word = "<C-j>" } },
+    event = "InsertEnter",
+  },
   prequire("john.coding.navigator"),
   { "windwp/nvim-ts-autotag", opts = { opts = { enable_close_on_slash = true } }, event = "VeryLazy" },
   { "echasnovski/mini.surround", opts = {}, event = "VeryLazy" },
@@ -10,17 +15,6 @@ return {
   { "gbprod/cutlass.nvim", keys = { { "m", mode = { "x", "n" } } }, opts = { cut_key = "m" } },
   { "echasnovski/mini.jump2d", opts = {}, event = "VeryLazy" },
   { "echasnovski/mini.jump", opts = {}, event = "VeryLazy" },
-  {
-    "supermaven-inc/supermaven-nvim",
-    opts = {
-      keymaps = {
-        accept_suggestion = "<M-j>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-j>",
-      },
-    },
-    event = "InsertEnter",
-  },
   prequire("john.coding.coerce"),
   prequire("john.coding.refactoring"),
 }
