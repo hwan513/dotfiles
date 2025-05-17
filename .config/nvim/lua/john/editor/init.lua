6604a227e5ae6aede34e56ef6c750a3dd198ed34
Henry Wang <wang.henryhan@gmail.com>
2025-05-18 09:31:54 +1200

    setup editor.lua

--- .config/nvim/lua/john/editor/init.lua
+++ .config/nvim/lua/john/editor/init.lua
@@ -1,13 +1,4 @@
 local prequire = require("config.utils").prequire
 return {
-  { "folke/which-key.nvim", event = "VeryLazy", opts = {} }, -- need to finish which-key config
-  { "nvimdev/dashboard-nvim", enabled = false, event = "VimEnter", opts = {} },
-  prequire("john.editor.auto-session"),
-  { "MagicDuck/grug-far.nvim", opts = {} },
-  {
-    "mbbill/undotree",
-    cmd = "UndotreeToggle",
-    keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" } },
-  },
   prequire("john.editor.multicursor"),
 }
