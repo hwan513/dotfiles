fcd0ea68237002539b37b5e5e003ac0b60e2eb4d
Henry Wang <wang.henryhan@gmail.com>
2025-05-17 22:52:25 +1200

    move mini-icons into libs.lua

--- .config/nvim/lua/john/ui/init.lua
+++ .config/nvim/lua/john/ui/init.lua
@@ -2,7 +2,6 @@ local prequire = require("config.utils").prequire
 return {
   -- yazi is on the radar
   prequire("john.ui.mini-hipatterns"),
-  prequire("john.ui.mini-icons"),
   prequire("john.ui.nvim-ufo"),
   {
     "stevearc/oil.nvim",
