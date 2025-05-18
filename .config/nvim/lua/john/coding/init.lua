56c9e29d432f3ab590eb933f832e6a2a5312ee44
Henry Wang <wang.henryhan@gmail.com>
2025-05-18 16:38:09 +1200

    add motions.lua

--- .config/nvim/lua/john/coding/init.lua
+++ .config/nvim/lua/john/coding/init.lua
@@ -5,6 +5,4 @@ return {
     opts = { keymaps = { accept_suggestion = "<M-j>", clear_suggestion = "<C-]>", accept_word = "<C-j>" } },
     event = "InsertEnter",
   },
-  { "echasnovski/mini.jump2d", opts = {}, event = "VeryLazy" },
-  { "echasnovski/mini.jump", opts = {}, event = "VeryLazy" },
 }
---
+++ .config/nvim/lua/plugins/motions.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "echasnovski/mini.ai",
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+          d = { "%f[%d]%d+" }, -- digits
+          e = { -- Word with case
+            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+            "^().*()$",
+          },
+          g = function() -- entire buffer
+            local from = { line = 1, col = 1 }
+            local to = { line = vim.fn.line("$"), col = math.max(vim.fn.getline("$"):len(), 1) }
+            return { from = from, to = to }
+          end,
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+        },
+      }
+    end,
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      modes = {
+        search = { enabled = true },
+        label = { min_pattern_length = 0 },
+      },
+    },
+    -- stylua: ignore start
+    keys = {
+      { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "<c-s>", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+    -- stylua: ignore end
+  },
+}
