71b03874953aed40c2409934ee3a97412fe8f623
Henry Wang <wang.henryhan@gmail.com>
2025-05-23 09:20:16 +1200

    fix(nvim): disable ltex by default

--- .config/nvim/lua/extras/integrations/markup.lua
+++ .config/nvim/lua/extras/integrations/markup.lua
@@ -1,3 +1,4 @@
+local ltex_enabled = false
 vim.lsp.config("ltex_plus", {
   settings = {
     ltex = {
@@ -12,10 +13,14 @@ vim.lsp.config("ltex_plus", {
 return {
   {
     "barreiroleo/ltex_extra.nvim",
+    enabled = ltex_enabled,
     branch = "dev",
     opts = { load_langs = { "en-NZ" } },
     ft = { "markdown", "tex", "text", "typst" },
   },
   { "OXY2DEV/markview.nvim", lazy = false },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim", opts = { ensure_installed = { "ltex_plus", "harper_ls" } } },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = { ensure_installed = { ltex_enabled and "ltex_plus" or nil, "harper_ls" } },
+  },
 }
