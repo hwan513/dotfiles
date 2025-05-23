09e36328bf353d869d132e58f705824865fb1f0c
Henry Wang <wang.henryhan@gmail.com>
2025-05-23 20:11:30 +1200

    feat(nvim): add code gen plugins

--- .config/nvim/lua/extras/ai.lua
+++ .config/nvim/lua/extras/ai.lua
@@ -4,4 +4,28 @@ return {
     opts = { keymaps = { accept_suggestion = "<M-j>", clear_suggestion = "<C-]>", accept_word = "<C-j>" } },
     event = "InsertEnter",
   },
+
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    config = function()
+      require("codecompanion").setup({
+        strategies = { chat = { adapter = "qwen3" }, inline = { adapter = "qwen3" }, cmd = { adapter = "qwen3" } },
+        adapters = {
+          qwen3 = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+              env = { url = "http://localhost:1234" },
+              schema = { model = { default = "qwen3-8b-dwq" } },
+            })
+          end,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "OXY2DEV/markview.nvim", opts = { preview = { filetypes = { "codecompanion" } } } },
+      { "saghen/blink.cmp", opts = { sources = { per_filetype = { codecompanion = { "codecompanion" } } } } },
+    },
+  },
 }
