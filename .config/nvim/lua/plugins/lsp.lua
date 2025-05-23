3cb4fa64f3442c0dc47e6419d2154fc302fc249f
Henry Wang <wang.henryhan@gmail.com>
2025-05-23 20:34:21 +1200

    feat(nvim): add nvim-lint

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     lazy = false, -- don't lazy load as this breaks auto install functionality
-    opts_extend = {"ensure_installed"},
+    opts_extend = { "ensure_installed" },
     opts = { auto_update = true },
     dependencies = {
       "mason-org/mason-lspconfig.nvim",
@@ -17,5 +17,18 @@ return {
     event = "BufWritePre",
     cmd = { "ConformInfo" },
     opts = { format_on_save = { timeout_ms = 500, lsp_format = "fallback" } },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    event = "LspAttach",
+    config = function(_, opts)
+      require("lint").linters_by_ft = opts.linters_by_ft
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "BufReadPost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
   },
 }
