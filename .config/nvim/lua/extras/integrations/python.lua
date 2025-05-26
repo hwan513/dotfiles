4c9b6ed5dc8335338a943ebb31dc232ff87aa7ac
Henry Wang <wang.henryhan@gmail.com>
2025-05-26 23:49:37 +1200

    feat(nvim): add ty into mason config

--- .config/nvim/lua/extras/integrations/python.lua
+++ .config/nvim/lua/extras/integrations/python.lua
@@ -1,8 +1,7 @@
 return {
   { "linux-cultist/venv-selector.nvim", branch = "regexp", cmd = { "VenvSelect" }, opts = {}, ft = "python" },
 
-  -- TODO: add ty to config once it has been merged into mason registry.
-  { "WhoIsSethDaniel/mason-tool-installer.nvim", opts = { ensure_installed = { "ruff" } } },
+  { "WhoIsSethDaniel/mason-tool-installer.nvim", opts = { ensure_installed = { "ruff", "ty" } } },
   {
     "stevearc/conform.nvim",
     opts = { formatters_by_ft = { python = { "ruff_fix", "ruff_format", "ruff_organize_imports" } } },
