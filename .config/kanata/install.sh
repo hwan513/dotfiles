b0a407b849298aa71437e32e0372789eb3abaa1c
Henry Wang <wang.henryhan@gmail.com>
2025-05-31 23:49:54 +1200

    fix(kanata): fix kanata tray installation

--- .config/kanata/install.sh
+++ .config/kanata/install.sh
@@ -7,13 +7,13 @@ rm Karabiner*.pkg
 /Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
 
 # Installation is being handled by mise
-# cargo binstall kanata
+cargo binstall kanata
 
 # Install kanata tray
-# gh release download -R "https://github.com/rszyma/kanata-tray.git" --pattern "*macos"
-# sudo chmod +x kanata-tray-macos
-# sudo chmod +x "$XDG_CONFIG_HOME/kanata/KanataTray/KanataTray"
-# mkdir -p "/Applications/KanataTray.app/Contents/MacOS/"
-# sudo cp -r "$XDG_CONFIG_HOME/kanata/KanataTray/" "/Applications/KanataTray.app/Contents/MacOS/"
-# mv kanata-tray-macos "/Applications/KanataTray.app/Contents/MacOS/kanata-tray-macos"
-# cp "$XDG_CONFIG_HOME/kanata/Info.plist" "/Applications/KanataTray.app/Contents/"
+gh release download -R "https://github.com/rszyma/kanata-tray.git" --pattern "*macos"
+sudo chmod +x kanata-tray-macos
+sudo chmod +x "$XDG_CONFIG_HOME/kanata/KanataTray/KanataTray"
+mkdir -p "/Applications/KanataTray.app/Contents/MacOS/"
+sudo cp -r "$XDG_CONFIG_HOME/kanata/KanataTray/" "/Applications/KanataTray.app/Contents/MacOS/"
+mv kanata-tray-macos "/Applications/KanataTray.app/Contents/MacOS/kanata-tray-macos"
+cp "$XDG_CONFIG_HOME/kanata/Info.plist" "/Applications/KanataTray.app/Contents/"
