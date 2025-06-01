b22fdba56322ac0ce1e160b72db8609c1a504a96
Henry Wang <wang.henryhan@gmail.com>
2025-06-02 11:15:05 +1200

    fix(hammerspoon): correctly implement AlertConnection

--- .config/hammerspoon/kanata.lua
+++ .config/hammerspoon/kanata.lua
@@ -1,7 +1,25 @@
--- Define the TCP host and port
+local function SaveLayers(filename)
+  print("Saving layers in: " .. filename)
+  print(hs.inspect.inspect(LayerTable))
+  io.open(filename, "w+"):write(hs.json.encode(LayerTable)):close()
+end
+
+local function LoadLayers(filename)
+  local layerFile = io.open(filename, "r")
+  if layerFile == nil then
+    return {}
+  end
+  local contents = layerFile:read("*a")
+  layerFile:close()
+  return hs.json.decode(contents) or {}
+end -- Define the TCP host and port
+
+-- Constants
 local host = "localhost"
 local port = 5829
-
+local layer_file = hs.configdir .. "/.kanata_layers.json"
+local default_layer = "dvorak"
+local recent_layer = default_layer
 local transientLayers = {
   fn1 = true,
   fn2 = true,
@@ -9,73 +27,62 @@ local transientLayers = {
   wm = true,
 }
 
-local function SaveLayers()
-  print("Saving layers")
-  print(hs.inspect.inspect(LayerTable))
-  io.open(".kanata_layers.json", "w+"):write(hs.json.encode(LayerTable)):close()
-end
-
-local function LoadLayers()
-  local layerFile = io.open(".kanata_layers.json", "r")
-  if layerFile == nil then
-    return {}
-  end
-
-  local contents = layerFile:read("*a")
-  layerFile:close()
-  if contents == nil then
-    return {}
-  end
-  return hs.json.decode(contents)
-end
-
-LayerTable = LoadLayers()
-DefaultLayer = "dvorak"
-RecentLayer = DefaultLayer
+LayerTable = LoadLayers(layer_file)
 
 OnRead = function(data)
   data = hs.json.decode(data)
   if TcpSocket ~= nil and data ~= nil and data.LayerChange ~= nil then
     local layer = data.LayerChange.new
     if layer ~= nil and not transientLayers[layer] then
-      RecentLayer = layer
+      recent_layer = layer
     end
     TcpSocket:read("\n")
   end
 end
 
-local alertConnection = function()
-  if not TcpSocket:connected() then
-    hs.alert.show("Hammerspoon is not connected to kanata socket")
-    TcpSocket:connect(host, port)
+local connect_kanata = function()
+  if TcpSocket == nil then
+    hs.alert.show("Error occured in hammerspoon while connecting to kanata")
+    return
   end
+  TcpSocket:connect(host, port, function()
+    hs.alert.show("Hammerspoon connected to kanata at port: " .. port)
+    TcpSocket:read("\n")
+  end)
 end
 
 -- Create a TCP socket and start the callback recursion
 TcpSocket = hs.socket.new(OnRead)
-TcpSocket:connect(host, port)
-TcpSocket:read("\n")
+connect_kanata()
+
+local alertConnection = function()
+  if not TcpSocket:connected() then
+    hs.alert.show("Hammerspoon failed to connect to kanata at port: " .. port)
+    hs.alert.show("Attempting to reconnect...")
+    connect_kanata()
+  end
+end
 
 -- Watch for changes in active application
 AppWatcher = hs.application.watcher.new(function(_, eventType, app)
   if eventType == hs.application.watcher.deactivated then
     -- Save layer setting on switch away unless it is the default layer
     if app:bundleID() ~= nil then
-      if RecentLayer == DefaultLayer then
+      if recent_layer == default_layer then
         LayerTable[app:bundleID()] = nil
-      elseif LayerTable[app:bundleID()] ~= RecentLayer then
-        LayerTable[app:bundleID()] = RecentLayer
+      elseif LayerTable[app:bundleID()] ~= recent_layer then
+        LayerTable[app:bundleID()] = recent_layer
       end
-      SaveLayers()
+      SaveLayers(layer_file)
     end
   -- Load layer setting on switch to an application
   elseif eventType == hs.application.watcher.activated then
     alertConnection()
     local newLayer = LayerTable[app:bundleID()]
     if newLayer == nil or newLayer == "" or newLayer == "noop" then
-      newLayer = DefaultLayer
+      newLayer = default_layer
     end
-    if newLayer ~= RecentLayer then
+    if newLayer ~= recent_layer then
       TcpSocket:write(hs.json.encode({ ChangeLayer = { new = newLayer } }))
     end
   end
