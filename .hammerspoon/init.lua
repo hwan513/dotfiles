6ecd413a26717a98dd854bd7f69ee0dc707799ec
Henry Wang <wang.henryhan@gmail.com>
2025-06-01 20:28:28 +1200

    refactor(hammerspoon): remove unused scripts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,3 @@
--- local basePath = os.getenv("HOME") .. "/.hammerspoon/"
 local function prequire(call)
   local status, _ = pcall(require, call)
   if not status then
@@ -6,14 +5,8 @@ local function prequire(call)
   end
 end
 
--- First install spoon install manually from the web
--- hs.loadSpoon("SpoonInstall")
--- spoon.SpoonInstall:andUse("EmmyLua")
-
--- prequire("menuUtils")
 prequire("kanata")
 prequire("keyUtils")
-prequire("finderOpen")
 prequire("clicker")
 prequire("mouselogger")
-prequire("reloadConfig") -- seems to have weird behaviour if not at the bottom
+prequire("reloadConfig") -- Seems to have weird behaviour if not at the bottom
