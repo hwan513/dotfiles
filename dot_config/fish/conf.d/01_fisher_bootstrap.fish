efb7d6a9663a418055a244abdc9873ba79cdb98b
Henry Wang <wang.henryhan@gmail.com>
2026-08-16 21:41:55 +1200

    feat(fish): extract into separate files

--- dot_config/fish/conf.d/01_fisher_bootstrap.fish
+++ dot_config/fish/conf.d/01_fisher_bootstrap.fish
@@ -1,5 +1,7 @@
-if status is-interactive; and not type -q fisher
-  echo "Fisher not installed, installing..."
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-  fisher update
-end
+status is-interactive || exit
+not type -q fisher || exit
+echo "Fisher not installed, installing..."
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+fisher update
+# apply tide configuration
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=No
